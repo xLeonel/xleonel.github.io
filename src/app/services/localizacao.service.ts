60652a7ad3a545849346a88e36b2495baa213af6
Leonel <vitorleonel100@hotmail.com>
2022-05-08 23:50:09 -0300

    map presenca

--- src/app/services/localizacao.service.ts
+++ src/app/services/localizacao.service.ts
@@ -9,24 +9,43 @@ export class LocalizacaoService {
   constructor() { }
 
   getLocation(): Promise<Localizacao | undefined> {
-    return new Promise<Localizacao | undefined>((resolve, reject) => {
-      if (!navigator.geolocation) {
-        reject(Error('No support for geolocation'));
-        return;
-      }
+    return new Promise<Localizacao | undefined>((resolve) => {
+      const navigatorLocationOptions = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
 
+      /* Determine browser permissions status */
       navigator.permissions.query({ name: 'geolocation' })
-        .then((perm) => {
-          if (perm.state.toLowerCase() === 'denied') {
+        .then((result) => {
+          /* result.state will be 'granted', 'denied', or 'error' */
+          if (result.state === 'granted') {
+            navigator.geolocation.getCurrentPosition(position => {
+              const longitude = position.coords.longitude;
+              const latitude = position.coords.latitude;
+              resolve(new Localizacao(latitude, longitude));
+
+            }, (error) => {
+              /* System/OS location services disabled */
+              console.log('System/OS services disabled', navigator);
+              resolve(undefined);
+            }, navigatorLocationOptions);
+
+          } else if (result.state.toLowerCase() === 'denied') {
+            resolve(undefined);
+          } else {
+            /* Browser location services disabled or error */
+            console.log('Browser location services disabled', navigator);
             resolve(undefined);
           }
-        })
+        }, (error) => {
+          /* Browser doesn't support querying for permissions */
+          console.log('Browser permissions services unavailable', navigator);
+          resolve(undefined);
+        }
+        );
 
-      navigator.geolocation.getCurrentPosition((position) => {
-        const longitude = position.coords.longitude;
-        const latitude = position.coords.latitude;
-        resolve(new Localizacao(latitude, longitude));
-      });
     });
   }
 }
